46409bbbfb9b9b3ec3899e41a8349a546b7676b7
Akash Khandelwal <skykhandelwal1999@gmail.com>
2021-08-18 23:25:39 +0530

    Updates

---
+++ precache-manifest.572c565a73271439f60d7a0349d4a052.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf21624196f91162e491192f18f5e5c4",
+    "url": "/NetflixClone/index.html"
+  },
+  {
+    "revision": "21c1f9212d85cb6ffc1c",
+    "url": "/NetflixClone/static/css/main.fb82f64a.chunk.css"
+  },
+  {
+    "revision": "d27f0a31ba6705bad404",
+    "url": "/NetflixClone/static/js/2.5c9ecf05.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/NetflixClone/static/js/2.5c9ecf05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21c1f9212d85cb6ffc1c",
+    "url": "/NetflixClone/static/js/main.1e3152f2.chunk.js"
+  },
+  {
+    "revision": "4e821e734f9e9aac872e",
+    "url": "/NetflixClone/static/js/runtime-main.cb66b7ab.js"
+  }
+]);
